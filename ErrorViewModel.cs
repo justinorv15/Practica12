9035ecbafb4491e5ca718501c3609a6816adbcfb
justinorv15 <70674647+justinorv15@users.noreply.github.com>
2020-10-24 13:29:49 -0500

    Add files via upload

---
+++ ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Practica12.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Practica12.Models;
+
+namespace Practica12.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string numero1, string numero2, string tipo)
+        {
+            int a = Convert.ToInt32(numero1);
+            int b = Convert.ToInt32(numero2);
+            int c = 0;
+            if (tipo=="Suma")
+            {
+                c=a+b;
+                ViewBag.Result = c;
+            }
+            else
+            {
+                ViewBag.Result = c;
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
